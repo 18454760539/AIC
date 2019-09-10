33a12baf5199e60c286180c29941f02ed5991280
鸿昊 <1037838286@qq.com>
2019-09-10 18:21:47 +0800

    添加项目文件。

---
+++ AIC/admin/start/json/menu.js
@@ -0,0 +1,148 @@
+{
+    "code": 0,
+        "msg": "",
+            "data": [
+                {
+                    "title": "主页",
+                    "icon": "layui-icon-home",
+                    "name": "",
+                    "jump": ""
+                },
+                {
+                    "title": "工厂管理",
+                    "icon": "layui-icon-component",
+                    "name": "gongchang",
+                    "list": [
+                        {
+                            "title": "工厂信息管理",
+                            "jump": "gongchang/factory"
+                        },
+                        {
+                            "title": "账单管理",
+                            "jump": "gongchang/yagubillmanagement"
+                        },
+                        {
+                            "title": "工厂集团管理",
+                            "jump": "gongchang/factoryGroupManagement"
+                        },
+                        {
+                            "title": "牙谷智能设备管理",
+                            "jump": "gongchang/yaguintelligenthardware"
+                        },
+                        {
+                            "title": "工厂访问数据分析",
+                            "jump": "gongchang/yaguFactoryDataAnalysis"
+                        },
+                        {
+                            "title": "工厂使用功能分析",
+                            "jump": "gongchang/factoryfunctionanalysis"
+                        }
+                    ]
+                },
+                {
+                    "title": "聆听/任务管理",
+                    "icon": "layui-icon-component",
+                    "name": "yijianjianyi",
+                    "list": [
+
+                        {
+                            "title": "聆听",
+                            "jump": "yijianjianyi/suggest"
+                        },
+                        {
+                            "title": "任务管理",
+                            "jump": "yijianjianyi/bugmanagement"
+                        }
+                    ]
+                },
+                {
+                    "title": "工作日志",
+                    "icon": "layui-icon-component",
+                    "name": "gongzuorizhi",
+                    "list": [
+
+                        {
+                            "title": "实施日志",
+                            "jump": "gongzuorizhi/shishirizhi"
+                        }, {
+                            "title": "客服日志",
+                            "jump": "gongzuorizhi/cslog"
+                        }, {
+                            "title": "业务日志",
+                            "jump": "gongzuorizhi/yewurizhi"
+                        }
+                    ]
+                },
+                {
+                    "title": "系统管理",
+                    "icon": "layui-icon-component",
+                    "name": "yijianjianyi",
+                    "list": [
+
+                        {
+                            "title": "员工管理",
+                            "jump": "gerenzhongxin/yuangongguanli"
+                        }
+                    ]
+                },
+                {
+                    "title": "平台管理",
+                    "icon": "layui-icon-component",
+                    "name": "pingtaiguanli",
+                    "list": [
+                        {
+                            "title": "平台版本管理",
+                            "jump": "pingtaiguanli/softwarevesionlist"
+                        },
+                        {
+                            "title": "平台权限管理",
+                            "jump": "pingtaiguanli/permissions"
+                        },
+                        {
+                            "title": "平台机房管理",
+                            "jump": "pingtaiguanli/computerroom"
+                        },
+                        {
+                            "title": "产品服务管理",
+                            "jump": "pingtaiguanli/product"
+                        },
+                        {
+                            "title": "数据库表结构查看器",
+                            "jump": "pingtaiguanli/alllistviewer"
+                        }, {
+                            "title": "APP升级管理",
+                            "jump": "pingtaiguanli/APPupgrademanagement"
+                        }
+                        , {
+                            "title": "字段解密",
+                            "jump": "pingtaiguanli/encryptanddecode"
+                        }
+                        , {
+                            "title": "重置平台数据库链接",
+                            "jump": "pingtaiguanli/ResetDatabaseLink"
+                        }
+                        , {
+                            "title": "平台新闻公告资讯",
+                            "jump": "pingtaiguanli/news"
+                        }
+                    ]
+                },
+                {
+                    "title": "平台调试",
+                    "icon": "layui-icon-component",
+                    "name": "pingtaiguanli",
+                    "list": [
+                        {
+                            "title": "http请求调试",
+                            "jump": "pingtaitiaoshi/httpqingqiu"
+                        },
+                        {
+                            "title": "平台异常管理",
+                            "jump": "pingtaitiaoshi/yichang"
+                        }
+                    ]
+                }
+            ]
+}
+
+
---
+++ denturebll/basemodels/user.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    ///<summary>
+    ///用户表
+    ///</summary>
+    public partial class user
+    {
+        public user()
+        {
+
+        }
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Desc:邮箱
+        /// Default:0
+        /// Nullable:True
+        /// </summary>           
+        public string email { get; set; }
+
+        /// <summary>
+        /// Desc:推荐人邮箱
+        /// Default:0
+        /// Nullable:True
+        /// </summary>           
+        public string tj_email { get; set; }
+
+        /// <summary>
+        /// Desc:密码
+        /// Default:0
+        /// Nullable:True
+        /// </summary>           
+        public string password { get; set; }
+
+        /// <summary>
+        /// Desc:二级密码
+        /// Default:0
+        /// Nullable:True
+        /// </summary>           
+        public string towpassword { get; set; }
+
+        /// <summary>
+        /// Desc:账户数量
+        /// Default:0
+        /// Nullable:True
+        /// </summary>    
+        public decimal? number { get; set; }
+
+        /// <summary>
+        /// Desc:注册时间
+        /// Default:0
+        /// Nullable:True
+        /// </summary>    
+        public DateTime addtime { get; set; }
+
+        /// <summary>
+        /// Desc:账号状态（0：正常，1：封禁）
+        /// Default:0
+        /// Nullable:True
+        /// </summary>    
+        public int? type { get; set; }
+    }
+}
